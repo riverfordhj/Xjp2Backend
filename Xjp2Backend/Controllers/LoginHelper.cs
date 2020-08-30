8a55db05c5468aa468007ad4e14554ef95a144b7
river_ford@qq.com <river_ford@qq.com>
2020-08-30 18:26:10 +0800

    clean

--- Xjp2Backend/Controllers/LoginHelper.cs
+++ Xjp2Backend/Controllers/LoginHelper.cs
@@ -4,8 +4,18 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Xjp2Backend.Models
+namespace Xjp2Backend.Controllers
 {
+    public class LoginRequest
+    {
+        [Required(ErrorMessage = "{0} is required.")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "{0} is required.")]
+        [StringLength(128, MinimumLength = 6, ErrorMessage = "minimum Length of {0} is 6.")]
+        public string Password { get; set; }
+    }
+
     public class RefreshTokenRequest
     {
         [Required(ErrorMessage = "{0} is required.")]
