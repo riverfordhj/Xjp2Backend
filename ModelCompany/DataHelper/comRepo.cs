cbe9f9981b067d87f9ccff7816759cca72a3a644
tjhotwind <1030494364@qq.com>
2021-11-30 11:21:06 +0800

    Revert "捕捉异常、更改营收税收分布"
    This reverts commit 2ae8ba69638aa0156fcdcd425c396299b0cdc9af.

--- ModelCompany/DataHelper/comRepo.cs
+++ ModelCompany/DataHelper/comRepo.cs
@@ -263,22 +263,17 @@ namespace ModelCompany.DataHelper
             string[] industryName = new string[]{ "", "农、林、牧、渔业", "采矿业", "制造业", "电力、燃气及水的生产和供应业", "建筑业", "交通运输仓储和邮政业", 
                 "信息传输、计算机服务和软件业", "批发和零售业", "住宿和餐饮业", "金融业", "房地产业", "租赁和商务服务业", "科学研究、技术服务和地质勘探业", 
                 "水利、环境和公共设施管理业", "居民服务和其他服务业", "教育", "卫生、社会保障和社会福利业", "文化体育和娱乐业" };
-                var companyCount = from tt in _context.CompanyTax.Where(cb => cb.CompanyBasicInfo.CompanyBuildings.BuildingName == buildingName && cb.Year == 2020)
-                                   group tt by tt.CompanyBasicInfo.IndustryCode into g
-                                   select new
-                                   {
-                                       g.Key,
-                                       industryName = industryName[int.Parse(g.Key)],
-                                       industryCompanyCount = g.Count(),
-                                       industryRevenue = g.Sum(tt => tt.Revenue).ToString("F2"),
-                                       industryTax = g.Sum(tt => tt.Tax).ToString("F2")
-                                   };
-                return companyCount;
-            }
-            catch (Exception e)
-            {
-                return null;
-            }
+            var companyCount = from tt in _context.CompanyTax.Where(cb => cb.CompanyBasicInfo.CompanyBuildings.BuildingName == buildingName && cb.Year == 2020)
+                               group tt by tt.CompanyBasicInfo.IndustryCode into g
+                               select new
+                               {
+                                   g.Key,
+                                   industryName = industryName[int.Parse(g.Key)],
+                                   industryCompanyCount = g.Count(),
+                                   industryRevenue = g.Sum(tt => tt.Revenue).ToString("F2"),
+                                   industryTax = g.Sum(tt => tt.Tax).ToString("F2")
+                               };
+            return companyCount;
         }
 
         //返回指定楼栋营收分布
